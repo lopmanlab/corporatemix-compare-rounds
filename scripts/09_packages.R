69913b07427b56b48d79347b3d4ad5f441c5b784
Moses Chapa Kiti <mchapak@gmail.com>
2023-03-29 09:49:58 -0400

    renamed

--- scripts/09_packages.R
+++
@@ -1,5 +0,0 @@
-library(dplyr)
-library(tidyr)
-packageurl <- "http://cran.r-project.org/src/contrib/Archive/socialmixr/socialmixr_0.1.8.tar.gz"
-install.packages(packageurl, repos=NULL, type="source")
-library(socialmixr)
