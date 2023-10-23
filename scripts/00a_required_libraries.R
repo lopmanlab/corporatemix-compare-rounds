7e1ae4b12a01ea4428efa71e8356ca0d373010db
Moses Chapa Kiti <mchapak@gmail.com>
2023-10-23 15:54:53 +0300

    moved to /old

--- scripts/00a_required_libraries.R
+++
@@ -1,6 +0,0 @@
-
-# install.packages("pacman") # install this package first if not installed
-
-pacman::p_load(
-  "reshape2", "ggplot2", "ggpubr", "dplyr", "tidyverse", "kableExtra",
-  "gridExtra", "RColorBrewer", "table1", "gtsummary", "Hmisc", "cowplot")
